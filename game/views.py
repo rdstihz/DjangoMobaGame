a0d91442c96b5c115f176692631bdf22aed90efe
Jiantao Zeng <rdstihz@qq.com>
2022-08-19 17:13:40 +0800

    write first website

--- game/views.py
+++ game/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+
+def index(request):
+    content = '''
+        <h1 style="text-align: center">主菜单</h1>
+        <a href="/play/">进入游戏页面</a>
+    '''
+    return HttpResponse(content)
+
+def play(request):
+    content = '''
+        <h1 style="text-align: center">游戏界面</h1>
+        <a href="/">返回主菜单</a>
+    '''
+    return HttpResponse(content)
+
